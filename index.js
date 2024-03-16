ddf895937974c3c20f1e614e1e31e8aae1df687c
Project 1.0 <163016882+proopz@users.noreply.github.com>
2024-03-17 01:17:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const secretsList = document.getElementById('secretsList');
 
 // Fetch secrets from GitHub Secrets
-const authHost = process.env.AUTH_HOST;
-const authUsername = process.env.AUTH_UN;
-const authToken = process.env.AUTHTOK;
-const authTarget = process.env.AUTH_TO;
+const authHost = "${{ secrets.AUTH_HOST }}";
+const authUsername = "${{ secrets.AUTH_UN }}";
+const authToken = "${{ secrets.AUTHTOK }}";
+const authTarget = "${{ secrets.AUTH_TO }}";
 
 // Display secrets on the web page
 secretsList.innerHTML += `<li>Authentication Host: ${authHost}</li>`;
 secretsList.innerHTML += `<li>Authentication Username: ${authUsername}</li>`;
 secretsList.innerHTML += `<li>Authentication Token: ${authToken}</li>`;
 secretsList.innerHTML += `<li>Authentication Target: ${authTarget}</li>`;
+
