c0ed64b405c05d7e5cd9bec0d4d30b2c8314bc36
Project 1.0 <163016882+proopz@users.noreply.github.com>
2024-03-23 00:35:14 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const secretsList = document.getElementById('secretsList');
-
-// Display secrets on the web page
-secretsList.innerHTML += `<li>Authentication Host: ${authHost}</li>`;
-secretsList.innerHTML += `<li>Authentication Username: ${authUsername}</li>`;
-secretsList.innerHTML += `<li>Authentication Token: ${authToken}</li>`;
-secretsList.innerHTML += `<li>Authentication Target: ${authTarget}</li>`;
-
